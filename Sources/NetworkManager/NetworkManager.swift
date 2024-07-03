8ebb0e792dc10db7b8d8bcb11d586b96d0ce40c5
Batuhan <32312869+batuhankucukyildiz@users.noreply.github.com>
2024-07-03 11:55:18 +0300

    Update NetworkManager.swift

--- Sources/NetworkManager/NetworkManager.swift
+++ Sources/NetworkManager/NetworkManager.swift
@@ -1,10 +1,3 @@
-//  NetworkManager.swift
-//
-//  Created by Batuhan Kucukyildiz on 15.01.2024.
-//
-
-import Foundation
-
 final public class NetworkManager {
     private init() {}
     public static let shared: NetworkManager = NetworkManager() // Singleton Pattern
@@ -27,12 +20,14 @@ final public class NetworkManager {
         }
         
         do {
-            return try decodeJSON(data: data)
+            let jsonData = try self.jsonDecoder.decode(T.self, from: data)
+            return jsonData
         } catch {
             throw error
         }
     }
 
+    
     private func handleNetworkRequest(response: HTTPURLResponse) async throws -> String {
         switch response.statusCode {
         case 200...299:
@@ -46,23 +41,4 @@ final public class NetworkManager {
         }
     }
 
-    // JSON verisini decode etme fonksiyonu
-    private func decodeJSON<T: Decodable>(data: Data) throws -> T {
-        let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
-
-        if let jsonArray = jsonObject as? [[String: Any]] {
-            let jsonData = try JSONSerialization.data(withJSONObject: jsonArray)
-            let decodedData = try self.jsonDecoder.decode([T].self, from: jsonData)
-            guard let firstElement = decodedData.first else {
-                throw NetworkError.invalidResponse(description: "No elements found in array")
-            }
-            return firstElement
-        } else if let jsonDict = jsonObject as? [String: Any] {
-            let jsonData = try JSONSerialization.data(withJSONObject: jsonDict)
-            let decodedData = try self.jsonDecoder.decode(T.self, from: jsonData)
-            return decodedData
-        } else {
-            throw NetworkError.invalidResponse(description: "Unexpected JSON format")
-        }
-    }
 }
